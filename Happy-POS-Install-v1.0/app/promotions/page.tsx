bd60943aeb1740073cf706c7a0506862cd8f0c6a
happydaddy <j.pasuknirun@gmail.com>
2026-04-03 12:45:11 +0700

    chore: remove legacy Happy POS installation directory and associated source files

--- Happy-POS-Install-v1.0/app/promotions/page.tsx
+++
@@ -1,8 +0,0 @@
-import { db } from '@/lib/db'
-import PromotionsClient from '@/components/PromotionsClient'
-
-export const dynamic = 'force-dynamic'
-
-export default function PromotionsPage() {
-  return <PromotionsClient initialPromotions={db.getPromotions()} />
-}
--- Happy-POS-Install-v1.0/next.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'standalone',
-  // Next.js 15+: moved out of experimental.serverComponentsExternalPackages
-  serverExternalPackages: [],
-}
-
-module.exports = nextConfig
